0e1ddc2dfece19209817efcd9369edee39f52e74
Henry Sanchez <henry@hsnyc.co>
2017-02-07 10:14:24 -0500

    Removed ID’s and added class names.

--- UFandShands/apollo/custom_banner.php
+++ UFandShands/apollo/custom_banner.php
@@ -12,28 +12,28 @@
 <h6>Fill out this form to learn more about our program</h6>
 
 <form id="contact_form1" class="banner_form" name="contact_form" action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?> method="post">
-        <ul> 
+        <ul>
             <li style="position: absolute; left: -5000px; top: -5000px;"><input id="mname" class="input_name1" type="text" name="name" value="" placeholder="Fill this field out if you aren't interested in any of the programs."></li>
-            <li><input id="fname" class="input_name1" type="text" name="first_name" value="First Name*"></li> 
-            <li><input id="lname" class="input_name1" type="text" name="last_name" value="Last Name*"></li>
-            <li><input id="email" class="input_name1" type="email" name="email" value="Email*"></li>
-            <li><input id="phone" class="input_name1" type="text" name="phone" value="Phone"></li>
- 
+            <li><input class="fname input_name1" type="text" name="first_name" value="First Name*"></li>
+            <li><input class="lname input_name1" type="text" name="last_name" value="Last Name*"></li>
+            <li><input class="email input_name1" type="email" name="email" value="Email*"></li>
+            <li><input class="phone input_name1" type="text" name="phone" value="Phone"></li>
+
 <li><div style="position:absolute; left:-9999px; top: -9999px;">
     <label for="pardot_extra_field">Comments</label>
     <input type="text" id="pardot_extra_field" name="pardot_extra_field"></div></li>
 
-           
+
             <li>
             <input type="submit" name="submit" value="Learn More" class="send_btn1" >
             </li>
-        </ul> 
+        </ul>
         <div class="clear"></div>
- 
-                 
+
+
     </form>
 </div>
-		</div>		
+		</div>
 	</div>
-</section>	
+</section>
 <!-- eof apollo banner -->
